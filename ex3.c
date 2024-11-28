4c9aa51843c20795f8a7fc64c77d7f98e656c680
Shalom Prinz <shalom.prinz@gmail.com>
2024-11-28 14:32:52 +0200

    Part 2 - daily data insertion

--- ex3.c
+++ ex3.c
@@ -24,7 +24,10 @@ char types[NUM_OF_TYPES][TYPES_NAMES] = {"SUV", "Sedan", "Coupe", "GT"};
 #define DELTAS 6
 #define DONE 7
 
-void printMenu(){
+// Used as uninitialized value for arrays (specifically, the cube array)
+#define DEFAULT_VALUE (-1)
+
+void printMenu() {
     printf("Welcome to the Cars Data Cube! What would you like to do?\n"
            "1.Enter Daily Data For A Brand\n"
            "2.Populate A Day Of Sales For All Brands\n"
@@ -35,14 +38,64 @@ void printMenu(){
            "7.exit\n");
 }
 
+/*
+ Gets cube and days arrays
+ Returns to menu if scanned carBrand is invalid
+ Scans daily sales for a specific brand, and inserts it to the cube
+*/
+void scanBrandDailySales(int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES], int days[NUM_OF_BRANDS]) {
+    int carBrand;
+    scanf("%d", &carBrand);
+    int brandDay = days[carBrand];
+
+    /*
+     Validates:
+        - carBrand is between 0 and max brand index available (= NUM_OF_BRANDS - 1)
+        - carBrand at brandDay isn't already set
+        // TODO Before submit - check their exe works by cube[][][0]
+    */
+    if (carBrand < 0 || carBrand > NUM_OF_BRANDS - 1 || cube[brandDay][carBrand][0] != DEFAULT_VALUE) {
+        printf("This brand is not valid\n");
+        return;
+    }
+
+    // Insert values of all types sales to given brand in cube
+    int dailySales[NUM_OF_TYPES] = {0};
+    for (int carType = 0; carType < NUM_OF_TYPES; carType++) {
+        scanf("%d", &dailySales[carType]);
+        cube[brandDay][carBrand][carType] = dailySales[carType];
+    }
+}
+
+/*
+ Gets cube, days and remainingBrands arrays
+ Sets remainingBrands to an array with this values format:
+    - 0 means uninitialized brand sales
+    - 1 means brand sales is initialized
+
+ Returns whether all brands are initialized, 1 is true and 0 is false
+*/
+int getUninitializedBrands(int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES], int days[NUM_OF_BRANDS],\
+    int remainingBrands[NUM_OF_BRANDS]) {
+    int isDataCompleted = 1;
+    for (int carBrand = 0; carBrand < NUM_OF_BRANDS; carBrand++) {
+        int brandDay = days[carBrand];
+        // TODO assuming first is all, like the todo in scanBrandDailySales()
+        if (cube[brandDay][carBrand][0] == DEFAULT_VALUE)
+            isDataCompleted = 0;
+        else
+            remainingBrands[carBrand] = 1;
+    }
+    return isDataCompleted;
+}
 
 int main() {
-    // Initialize all cube values to (-1)s
+    // Initialize all cube values to default values
     int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES];
     for (int i = 0; i < DAYS_IN_YEAR; i++) {
         for (int j = 0; j < NUM_OF_BRANDS; j++) {
             for (int k = 0; k < NUM_OF_TYPES; k++) {
-                cube[i][j][k] = -1;
+                cube[i][j][k] = DEFAULT_VALUE;
             }
         }
     }
@@ -55,31 +108,31 @@ int main() {
     while (choice != DONE) {
         switch(choice) {
             case ADD_ONE:
-                printf("Enter a car brand, followed by 4 ints representing daily sales sum per brand:\n");
-                int carBrand;
-                scanf("%d", &carBrand);
-
-                // Validate carBrand is between 0 and max brand index available (= NUM_OF_BRANDS - 1)
-                if (carBrand < 0 || carBrand > NUM_OF_BRANDS - 1) {
-                    printf("This brand is not valid\n");
-                    printMenu();
-                    return 0;
-                }
-
-                // Insert values of all types sales to given brand in cube
-                int brandDay = days[carBrand];
-                int dailySales[NUM_OF_TYPES] = {0};
-                for (int carType = 0; carType < NUM_OF_TYPES; carType++) {
-                    scanf("%d", &dailySales[carType]);
-                    cube[brandDay][carBrand][carType] = dailySales[carType];
-                }
+                scanBrandDailySales(cube, days);
                 break;
             case ADD_ALL:
-                //...
+                // Init remainingBrands values to 0s, where 0 means uninitialized brand
+                int remainingBrands[NUM_OF_BRANDS] = {0};
+                // Verify if data is completed, meaning no brands remained without sales data
+                int isDataCompleted = getUninitializedBrands(cube, days, remainingBrands);
+
+                // Scan brand sales until all brands are scanned
+                while (isDataCompleted == 0) {
+                    printf("No data for brands");
+                    for (int i = 0; i < NUM_OF_BRANDS; i++) {
+                        if (remainingBrands[i] == 0)
+                            printf(" %s", brands[i]);
+                    }
+                    printf("\nPlease complete the data\n");
+                    scanBrandDailySales(cube, days);
+                    isDataCompleted = getUninitializedBrands(cube, days, remainingBrands);
+                }
+
+                // Increase days counter
+                for (int i = 0; i < NUM_OF_BRANDS; i++) {
+                    days[i]++;
+                }
                 break;
-            /*
-             ......
-             */
             default:
                 printf("Invalid input\n");
         }
