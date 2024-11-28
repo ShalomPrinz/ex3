2a1f870196e514f427a79e9292f1fc41ab76830f
Shalom Prinz <shalom.prinz@gmail.com>
2024-11-28 09:58:16 +0200

    Part 0 - init project

--- ex3.c
+++ ex3.c
@@ -6,6 +6,7 @@ Assignment: ex3
 
 #include <stdio.h>
 
+// Assignment given constants
 #define NUM_OF_BRANDS 5
 #define BRANDS_NAMES 15
 #define NUM_OF_TYPES 4
@@ -15,7 +16,13 @@ Assignment: ex3
 char brands[NUM_OF_BRANDS][BRANDS_NAMES] = {"Toyoga", "HyunNight", "Mazduh", "FolksVegan", "Key-Yuh"};
 char types[NUM_OF_TYPES][TYPES_NAMES] = {"SUV", "Sedan", "Coupe", "GT"};
 
-const int addOne = 1, addAll = 2, stats = 3, print = 4, insights = 5, deltas = 6, done = 7;
+#define ADD_ONE 1
+#define ADD_ALL 2
+#define STATS 3
+#define PRINT 4
+#define INSIGHTS 5
+#define DELTAS 6
+#define DONE 7
 
 void printMenu(){
     printf("Welcome to the Cars Data Cube! What would you like to do?\n"
@@ -30,17 +37,27 @@ void printMenu(){
 
 
 int main() {
+    // Initialize all cube values to (-1)s
     int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES];
+    for (int i = 0; i < DAYS_IN_YEAR; i++) {
+        for (int j = 0; j < NUM_OF_BRANDS; j++) {
+            for (int k = 0; k < NUM_OF_TYPES; k++) {
+                cube[i][j][k] = -1;
+            }
+        }
+    }
+
+    // Days array to keep track of the day for each brand
     int days[NUM_OF_BRANDS] = {0};
     int choice;
     printMenu();
     scanf("%d", &choice);
-    while(choice != done){
-        switch(choice){
-            case addOne:
+    while(choice != DONE) {
+        switch(choice) {
+            case ADD_ONE:
                 //...
                 break;
-            case addAll:
+            case ADD_ALL:
                 //...
                 break;
             /*
